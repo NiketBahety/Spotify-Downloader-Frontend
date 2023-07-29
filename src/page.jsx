a771a0bdfbd44db71509e1694bfcb5041f96b6e9
NiketBahety <niketbahety@gmail.com>
2023-07-29 12:59:25 +0530

    Upload

---
+++ src/page.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Page() {
+  const [URL, setURL] = useState();
+
+  const hanldeClick = async () => {
+    let id = URL.split("/");
+    id = id[id.length - 1];
+    // window.location.href = `http://localhost:8000?id=${id}`;
+    window.location.href = `https://spotify-downloader-ryxm.onrender.com?id=${id}`;
+  };
+
+  return (
+    <div className="cont">
+      <input
+        type="text"
+        onChange={(e) => {
+          setURL(e.target.value);
+        }}
+        className="inp"
+        placeholder="Enter spotify playlist URL or id"
+      ></input>
+      <button onClick={() => hanldeClick()}>Download playlist</button>
+    </div>
+  );
+}
+
+export default Page;
